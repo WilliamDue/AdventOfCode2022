5be5aa2f270923b6c317de9d80f7a1b1e5d1d66b
due <williamhenrichdue@gmail.com>
2022-12-04 18:27:07 +0100

    Day 4 is done.

---
+++ day04/src/Main.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import Data.List.Split ( splitOn )
+
+type Range = (Int, Int)
+
+parse :: String -> [(Range, Range)]
+parse = map parseLine . lines
+    where toTuple :: [a] -> (a, a)
+          toTuple [a, b] = (a, b)
+          toTuple _ = error "List must only contain two elements."
+
+          parseRange :: String -> Range
+          parseRange = toTuple . map read . splitOn "-"
+
+          parseLine :: String -> (Range, Range)
+          parseLine = toTuple . map parseRange . splitOn ","
+
+isContained :: Range -> Range -> Bool
+isContained (a, b) (c, d) = (c <= a && b <= d) || (a <= c && d <= b)  
+
+isOverlap :: Range -> Range -> Bool
+isOverlap (a, b) (c, d) = signum (b - c) /= signum (a - d)
+
+solve1 :: [(Range, Range)] -> Int
+solve1 = length . filter (uncurry isContained)
+
+solve2 :: [(Range, Range)] -> Int
+solve2 = length . filter (uncurry isOverlap)
+
+main :: IO ()
+main = do
+    contents <- getContents
+    print . solve1 $ parse contents
+    print . solve2 $ parse contents
