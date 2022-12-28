1609120ef2acfb647707aaabf35c61acacda8075
due <williamhenrichdue@gmail.com>
2022-12-28 14:49:57 +0100

    Did a hecking brute force

--- day16/src/Main.hs
+++ day16/src/Main.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE TupleSections #-}
 module Main where
 
+import Debug.Trace
 import qualified Data.Map as M
 import Data.List.Split (splitOn)
 import qualified Data.Char as C
 import qualified Data.List as L
-import Data.Bifunctor (first, second)
+import Data.Bifunctor (first, second, bimap)
+import Data.Maybe ( fromJust, isNothing ) 
 
 data Node = Node { key :: String,
                    weight :: Int,
@@ -14,6 +16,10 @@ data Node = Node { key :: String,
 
 type Graph = M.Map String Node
 data Tree a = TreeNode a [Tree a] | Leaf deriving (Show, Eq)
+
+instance Functor Tree where
+    fmap f (TreeNode a ls) = TreeNode (f a) (fmap (fmap f) ls)
+    fmap f Leaf = Leaf
 
 parseLine :: String -> Node
 parseLine str = Node { key = key', weight = weight', neighbours = neighbours', isOpen = False }
@@ -48,29 +54,62 @@ maxNodes graph source maxLength = M.toList pathLengths'
 updateIsOpen :: Graph -> String -> Graph 
 updateIsOpen = flip $ M.adjust (\node -> node { isOpen = True })
 
-helper :: Int -> Graph -> String -> Int -> Tree Int
-helper maxLength graph source pathLength
+mkTree :: Int -> Graph -> String -> Int -> Tree (String, Int)
+mkTree maxLength graph source pathLength
     | maxLength < pathLength = Leaf
-    | null nodes = TreeNode (weight' * (maxLength - pathLength)) [Leaf]
-    | otherwise = TreeNode (weight' * (maxLength - pathLength)) $ zipWith3 (helper maxLength) graphs keys pathLengths'
+    | null nodes = TreeNode (source, (weight' * (maxLength - pathLength))) [Leaf]
+    | otherwise = TreeNode (source, (weight' * (maxLength - pathLength))) trees
     where nodes = maxNodes graph source (maxLength - pathLength)
           (keys, pathLengths) = unzip nodes
           graphs = map (updateIsOpen graph) keys
           pathLengths' = map (+pathLength) pathLengths
           weight' = weight $ graph M.! source
+          trees = zipWith3 (mkTree maxLength) graphs keys pathLengths'
 
 treeSum :: Tree Int -> Int
 treeSum (TreeNode a ls) = (+a) . maximum $ map treeSum ls
 treeSum Leaf = 0
 
-solve1 :: Graph -> Int
-solve1 graph = treeSum $ helper 30 graph "AA" 0
+treeScan :: (a -> b -> b) -> b -> Tree a -> Tree b
+treeScan _ _ Leaf = Leaf
+treeScan f lst (TreeNode a ls) = TreeNode b (map (treeScan f (f a lst)) ls)
+    where b = f a lst
 
+root :: Tree a -> a
+root (TreeNode a _) = a
+root Leaf = error "Could not get root."
+
+treeScan1 :: (a -> a -> a) -> Tree a -> Tree a
+treeScan1 f tree = treeScan f a tree
+    where a = root tree
+
+solve1 :: Graph -> Int
+solve1 graph = treeSum . fmap snd $ mkTree 30 graph "AA" 0
+
+subTrees :: Tree a -> [Tree a]
+subTrees (TreeNode _ ls) = ls
+subTrees Leaf = []
+
+paths :: Tree a -> [[a]]
+paths tree = helper $ treeScan (:) [] tree
+    where helper (TreeNode a ls) = a : concatMap helper ls
+          helper Leaf = []
+
+maxComb paths' = L.maximumBy (\(a, b) (c, d) -> (snd a + snd b) `compare` (snd c + snd d)) $ map (\p -> (p, max' $ filter (all (`notElem` (fst p)) . fst) paths')) paths'
+    where max' = L.maximumBy (\a b -> snd a `compare` snd b)
+
+collapse :: [(String, Int)] -> ([String], Int)
+collapse = second sum . unzip
+
+solve2 graph = uncurry (+)
+               . bimap snd snd 
+               . maxComb
+               . map (collapse . init) 
+               . paths 
+               $ mkTree 26 graph "AA" 0
 
 main :: IO ()
 main = do
     input <- parse <$> getContents
     print $ solve1 input
-    -- mapM_ putStrLn $ M.keys input
-    -- mapM_ putStr $ map (\(k, n) -> unlines . map ((k ++ " ") ++) $ neighbours n) $ M.toList input
-    -- print $ input
+    print $ solve2 input
